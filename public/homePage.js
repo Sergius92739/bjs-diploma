c2d009a0d7c0a30abdc6f0d9016525b4e97582b3
Sergius92739 <sergem279@gmail.com>
2023-04-28 11:57:59 +0300

    initial commit

---
+++ public/homePage.js
@@ -0,0 +1,102 @@
+'use strict'
+
+// Выход из личного кабинета
+const logoutButton = new LogoutButton();
+logoutButton.action = () => {
+    ApiConnector.logout((response) => {
+        if (response.success) {
+            location.reload();
+        }
+    })
+}
+
+// Получение информации о пользователе
+ApiConnector.current((response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+})
+
+// Получение текущих курсов валюты
+const ratesBoard = new RatesBoard();
+ratesBoard.obtainsCurrencyRates = () => {
+    ApiConnector.getStocks((response) => {
+        if (response.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+        }
+    })
+}
+ratesBoard.obtainsCurrencyRates();
+setInterval(ratesBoard.obtainsCurrencyRates, 60000);
+
+// Операции с деньгами
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Баланс поплнен");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        }
+    })
+}
+
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Конвертация произведена");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        }
+    })
+}
+
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, "Перевод произведен");
+        } else {
+            moneyManager.setMessage(response.success, response.error);
+        }
+    })
+}
+
+// Работа с избранным
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites((response) => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    }
+})
+
+favoritesWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, (response) => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, "Пользователь добавлен")
+        } else {
+            favoritesWidget.setMessage(response.success, response.error);
+        }
+    })
+}
+
+favoritesWidget.removeUserCallback = (id) => {
+    ApiConnector.removeUserFromFavorites(id, (response) => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, "Пользователь удалён")
+        } else {
+            favoritesWidget.setMessage(response.success, response.error);
+        }
+    })
+}
